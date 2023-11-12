a60b5d3d128a99e94d5050eff6650a9b85509439
prasanth7890 <maryratnamp@gmail.com>
2023-11-12 17:27:08 +0530

    added axios and connected to backend

--- client/src/Pages/Chatpage.js
+++ client/src/Pages/Chatpage.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 const Chatpage = () => {
+    const [chats, setChats] = useState([]);
+
+    useEffect(()=>{
+        fetchChats();
+    }, [])
+
+    const fetchChats = async() => {
+        const {data} = await axios.get('http://localhost:4000/chats');
+        console.log(data);
+        setChats(data);
+    }
+
   return (
     <div>
       This is chatpage
+      {chats.map((chat)=>{
+        <div>hello</div>
+      })}
     </div>
   )
 }
