6a0ca3b0b70082c2247f7fac34f75c8669e01c98
prasanth7890 <maryratnamp@gmail.com>
2023-11-12 16:30:08 +0530

    added .env file

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const dotenv = require('dotenv')
 const cors = require("cors");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const chats = require("./data/data");
+
+dotenv.config();
+const PORT = process.env.PORT;
+
 
 app.use(cors());
 
@@ -14,10 +20,18 @@ const io = new Server(server, {
     },
 });
 
-PORT=4000
 
 app.get('/', (req, res)=>{
     res.send("<h1>Hello world</h1>");
+});
+
+app.get('/chats', (req, res)=>{
+    res.json(chats);
+});
+
+app.get('/chats/:id', (req, res)=>{
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
 });
 
 
@@ -31,7 +45,6 @@ io.on('connection', (socket)=>{
     })
 
 })
-
 
 
 server.listen(PORT, ()=>{
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { io } from "socket.io-client";
 
 const socket = io("http://localhost:4000");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [receivedMessage, setReceivedMessage] = useState("");
+
   useEffect(()=>{
     socket.on("receive_message", (data)=>{
-      alert(data.message);
+      setReceivedMessage(data.message);
     });
   }, [socket]);
 
   const sendMessage = ()=> {
-      socket.emit("send_message", {
-        "message": `Hello from client: ${socket.id}` 
-      });
+    socket.emit("send_message", { message });
   }
 
 
   return (
     <div className="App">
       <h1>Hello world</h1>
-      <input type="text" placeholder='Message...' />
+      <input type="text" onChange={(event)=>setMessage(event.target.value)} placeholder='Message...' />
       <button onClick={sendMessage}>Send Message</button>
+      <h2>{receivedMessage}</h2>
     </div>
   );
 }
