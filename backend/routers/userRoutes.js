85df8282279a4235654c6e1e90b39b7d2c952b5b
prasanth7890 <maryratnamp@gmail.com>
2023-11-16 23:43:47 +0530

    created controllers & Routes for login & registration

--- backend/routers/userRoutes.js
+++ backend/routers/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const {registerUser} = require('../controller/userControllers');
+const {registerUser, authUser} = require('../controller/userControllers');
 
 const router = express.Router();
 
 router.route('/').post(registerUser);
-// router.route('/login').post(authUser);
+router.route('/login').post(authUser);
 
 module.exports = router;
